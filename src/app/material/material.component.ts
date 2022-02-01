21097987ba7b6f9a5c0496b6d815996028ddb333
ANDREW MILLER <H5730832@rl.gov>
2022-02-01 07:40:49 -0800

    implemented side mode

--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -1,5 +1,6 @@
-import { Component, HostBinding } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Component, HostBinding, OnDestroy } from '@angular/core';
+import { BehaviorSubject, fromEvent, Observable, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 
 type NavLink = {
   name: string,
@@ -22,16 +23,40 @@ const navLinkSource$ = new BehaviorSubject<NavLink[]>([
   templateUrl: './material.component.html',
   styleUrls: ['./material.component.scss']
 })
-export class MaterialComponent {
-  navLinks$ = navLinkSource$;
+export class MaterialComponent implements OnDestroy {
+  navLinks$: Observable<NavLink[]> = navLinkSource$;
 
   @HostBinding('class') classTheme: Theme = 'light';
 
   get mode() {
     return this._mode;
   }
-  set mode(v: Theme) {
-    this.classTheme = this._mode = v;
+  set mode(v: 'side' | 'over') {
+    this._mode = v;
   }
-  private _mode: Theme = 'light';
+  private _mode: 'side' | 'over' = 'side';
+
+  get theme() {
+    return this._theme;
+  }
+  set theme(v: Theme) {
+    this.classTheme = this._theme = v;
+  }
+  private _theme: Theme = 'light';
+
+  private _destroying$ = new Subject<void>();
+
+  constructor() {
+    fromEvent(window, 'resize').pipe(
+      tap((evt: Event) => {
+        this.mode = 1000 < window.innerWidth ? 'over' : 'side';
+      }),
+      takeUntil(this._destroying$)
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
 }
